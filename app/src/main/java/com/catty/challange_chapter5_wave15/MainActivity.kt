0001655268882ddb2f3cfb166f6fc51449d730fd
yordawira <50675414+yordawira@users.noreply.github.com>
2022-10-02 15:37:22 +0700

    Initial commit

---
+++ app/src/main/java/com/catty/challange_chapter5_wave15/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.catty.challange_chapter5_wave15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
